6ccce56560941d05b82f3a13e3baa662f37c00c3
Tejasvi Raghuvanshi <tejasviraghuvanshi@gmail.com>
2024-03-06 12:58:55 +0530

    Time: 97 ms (59.02%), Space: 28.4 MB (81.01%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.length();
+        if(n == 1 || n%2==1) 
+            return false;
+        int x=0;
+        for(int i=0 ; i<n ; i++){
+            if(s[i]=='(' || locked[i]=='0'){
+                x++;
+            }
+            else if(x>0){
+                x--;
+            }
+            else{
+                return false;
+            }
+        }
+         x=0;
+        for(int i=n-1 ; i>0 ; i--){
+            if(s[i]==')' || locked[i]=='0'){
+                x++;
+            }
+            else if(x>0){
+                x--;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+};
